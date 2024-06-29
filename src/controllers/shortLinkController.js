f9df6091072063ceeb261a3bcea99b6a688bde8f
cyberkillua <moshood988@gmail.com>
2024-06-29 12:03:15 +0100

    added get stats

--- src/controllers/shortLinkController.js
+++ src/controllers/shortLinkController.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4545;
 export const encode = async (req, res) => {
   try {
     const { originalURL } = req.body;
-    const shortCode = (counter++).toString(36); 
+    const shortCode = (counter++).toString(36);
     urlDatabase[shortCode] = originalURL;
     const shortURL = `http://localhost:${PORT}/${shortCode}`;
     res.status(200).json({ message: "Link shorten", shortURL });
@@ -31,7 +31,30 @@ export const decode = async (req, res) => {
       res.status(422).json({ error: "Original URL does not exist" });
       return;
     }
-    res.status(200).json({ message: "Link shorten", originalURL });
+    res.status(200).json({ message: "Link decoded", originalURL });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Request could not be completed",
+      error: error.message,
+    });
+  }
+};
+
+export const getStats = async (req, res) => {
+  try {
+    const { url_path } = req.query;
+    const originalURL = getOriginalURL(url_path);
+    if (originalURL === null) {
+      res.status(422).json({ error: "Original URL does not exist" });
+      return;
+    }
+    const stats = {
+      originalURL,
+      url_path,
+      encodedURL: `http://localhost:${PORT}/${url_path}`,
+    };
+    res.status(200).json({ message: "fetch successful", stats });
   } catch (error) {
     console.log(error);
     res.status(500).json({
--- src/routes/shortLink.routes.js
+++ src/routes/shortLink.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { decode, encode } from "../controllers/shortLinkController.js";
+import {
+  decode,
+  encode,
+  getStats,
+} from "../controllers/shortLinkController.js";
 
 const router = express.Router();
 
 router.post("/encode-url", encode);
 
 router.post("/decode-url", decode);
+
+router.get("/statistic", getStats);
 
 export default router;
