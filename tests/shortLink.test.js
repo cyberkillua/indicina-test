84a953d2f318492c8a20f606aad21fdeeaa162ef
cyberkillua <moshood988@gmail.com>
2024-06-29 14:44:16 +0100

    tests done

--- tests/shortLink.test.js
+++ tests/shortLink.test.js
@@ -10,7 +10,11 @@ import supertest, { agent as request } from "supertest";
 import { Request, Response, NextFunction } from "express";
 import * as sinon from "sinon";
 
-import { encode } from "../src/controllers/shortLinkController";
+import {
+  encode,
+  decode,
+  getStats,
+} from "../src/controllers/shortLinkController.js";
 
 let app;
 
@@ -47,5 +51,84 @@ describe("encode URL", () => {
       .send(reqBody);
 
     expect(response.status).toBe(200);
+  });
+});
+
+describe("decode URL", () => {
+  let request;
+
+  beforeEach(() => {
+    request = supertest(app);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it("decode should be defined", () => {
+    expect(decode).toBeDefined();
+  });
+  it("encode url should be successfully", async () => {
+    // First, encode a URL
+    const encodeResponse = await request
+      .post("/api/v1/shortlink/encode-url")
+      .send({ originalURL: "https://www.example.com" });
+
+    const shortURL = encodeResponse.body.shortURL;
+
+    // Now, decode the short URL
+    const decodeResponse = await request
+      .post("/api/v1/shortlink/decode-url")
+      .send({ encodedURL: shortURL });
+
+    expect(decodeResponse.status).toBe(200);
+    expect(decodeResponse.body).toHaveProperty("message", "Link decoded");
+    expect(decodeResponse.body).toHaveProperty(
+      "originalURL",
+      "https://www.example.com"
+    );
+  });
+  it("encode url should return 422 if encodedURL doesnt exist", async () => {
+    const response = await request
+      .post("/api/v1/shortlink/decode-url")
+      .send({ encodedURL: "https://www.example.com" });
+
+    expect(response.status).toBe(422);
+  });
+});
+
+describe("get stats", () => {
+  let request;
+
+  beforeEach(() => {
+    request = supertest(app);
+    jest.clearAllMocks();
+  });
+  it("decode should be defined", () => {
+    expect(getStats).toBeDefined();
+  });
+
+  it("should return stats for a valid url_path", async () => {
+    const encodeResponse = await request
+      .post("/api/v1/shortlink/encode-url")
+      .send({ originalURL: "https://www.example.com" });
+
+    const shortURL = encodeResponse.body.shortURL;
+
+    const url_path = shortURL.split("/").pop();
+    const originalURL = "https://www.example.com";
+
+    const response = await request.get(
+      `/api/v1/shortlink/statistic?url_path=${url_path}`
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("message", "fetch successful");
+    expect(response.body).toHaveProperty("stats");
+    expect(response.body.stats).toEqual({
+      originalURL,
+      url_path,
+      encodedURL: expect.stringContaining(url_path),
+    });
   });
 });
